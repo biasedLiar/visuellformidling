d59809f618d7740d3c31789bf8aaeb8897dc3742
BiasedLiar <Bretagard12345>
2024-09-10 22:58:39 +0200

    Moved animation to smaller scene

--- scene.py
+++ scene.py
@@ -19,7 +19,8 @@ class CreateCircle(Scene):
         self.play(FadeOut(c))
 
     def animate_path(self, points):
-        person = Star(outer_radius=0.2, color=YELLOW).set_fill(YELLOW, opacity=1)
+        person = Star(outer_radius=0.2, color=BLACK).set_fill(YELLOW, opacity=1)
+        #TODO move star to front
         person.move_to(points[0])
         self.add(person)
         lines = []
@@ -48,33 +49,35 @@ class CreateCircle(Scene):
         p8 = [right, 0, 0]
         p9 = [right, down, 0]
 
-        line_1_points = [p3, p2, p1, p4, p5, p8, p9, p6, p5]
+        line_1_points = [p3, p1, p4, p5, p8, p9, p6, p5]
         self.animate_path(line_1_points)
 
-        line_2_points = [p7, p8, p9, p3, p2, p1, p4, p5, p6]
+        line_2_points = [p7, p9, p3, p1, p4, p6]
         self.animate_path(line_2_points)
 
-        line_3_points = [p8, p7, p4, p5, p8, p9, p3, p2, p1]
+        line_3_points = [p8, p7, p4, p5, p8, p9, p3, p1]
         self.animate_path(line_3_points)
 
 
     def create_map(self):
-        background = Rectangle(height=4, width=8).set_color(GREEN_D)
+
+        vg = VGroup()
+        background = Rectangle(height=4, width=6).set_color(GREEN_D)
         background.set_fill(GREEN_D, opacity=1)
-        river = Rectangle(height=2, width=8).set_color(BLUE).set_fill(BLUE, opacity=1)
+        vg.add(background)
+
+        river = Rectangle(height=2, width=6).set_color(BLUE).set_fill(BLUE, opacity=1)
+        vg.add(river)
+
         island = RoundedRectangle(width=3, height=1.3, corner_radius=0.2) \
             .set_fill(GREEN_D, opacity=1).set_stroke(GREEN_D).shift(0.9*LEFT)
-        island2 = RoundedRectangle(width=3, height=1.3, corner_radius=0.2) \
+        island2 = RoundedRectangle(width=2, height=1.3, corner_radius=0.2) \
             .set_fill(GREEN_D, opacity=1).set_stroke(GREEN_D).align_to(background, RIGHT)
         island2_5 = Rectangle(width=1, height=1.3) \
             .set_fill(GREEN_D, opacity=1).set_stroke(GREEN_D).align_to(background, RIGHT)
+        vg.add(island, island2, island2_5)
 
 
-        self.add(background)
-        self.add(river)
-        self.add(island)
-        self.add(island2)
-        self.add(island2_5)
 
         bridge1 = Rectangle(height=0.7, width=0.3).set_fill(DARK_BROWN, opacity=1).set_stroke(DARK_BROWN).shift(1.3*LEFT + 0.8*DOWN)
         bridge2 = Rectangle(height=0.7, width=0.3).set_fill(DARK_BROWN, opacity=1).set_stroke(DARK_BROWN).shift(0.4*LEFT + 0.8*DOWN)
@@ -83,17 +86,10 @@ class CreateCircle(Scene):
         bridge5 = Rectangle(height=0.7, width=0.3).set_fill(DARK_BROWN, opacity=1).set_stroke(DARK_BROWN).shift(1.8*RIGHT + 0.8*DOWN)
         bridge6 = Rectangle(height=0.7, width=0.3).set_fill(DARK_BROWN, opacity=1).set_stroke(DARK_BROWN).shift(1.8*RIGHT + 0.8*UP)
         bridge7 = Rectangle(height=0.3, width=0.7).set_fill(DARK_BROWN, opacity=1).set_stroke(DARK_BROWN).shift(0.8*RIGHT)
+        vg.add(bridge1, bridge2, bridge3, bridge4, bridge5, bridge6, bridge7)
 
 
 
-
-        self.add(bridge1)
-        self.add(bridge2)
-        self.add(bridge3)
-        self.add(bridge4)
-        self.add(bridge5)
-        self.add(bridge6)
-        self.add(bridge7)
         '''
         a = Circle()
         b = Square()
@@ -102,16 +98,21 @@ class CreateCircle(Scene):
         self.play(ReplacementTransform(b, c))
         self.play(FadeOut(c))
         '''
+        return vg
 
 
     def construct(self):
         #self.camera.background_color = BLUE
         #self.wait(0.5)  # wait for 0.5 seconds
-        bridges = ImageMobject("images/Bridges.png")
         #bridges.height = 2
         #self.add(bridges)
-        self.create_map()
+        map = self.create_map()
+        self.add(map)
+        text = Text('De 7 broene av Königsberg').scale(1).shift(UP*3)
+        self.add(text)
         self.draw_paths()
+        self.play(map.animate.to_edge(RIGHT))
         self.transform()
+
 
 
